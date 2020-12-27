f94568d63a9de9d5172eae5b57343ee9f5e391d0
LuoZhiBinGG <3028640780@qq.com>
2020-12-27 15:55:33 +0800

    学习了回旋数组

---
+++ a0_5_数组中的算法集合/src/求数值型数组元素的MaxMinAverageSummation/Evaluationoperation.java
@@ -0,0 +1,13 @@
+package 求数值型数组元素的MaxMinAverageSummation;
+
+/*
+ *  算法的考察：求数值型数组元素的最大值、最小值、平均数和总和等。
+ *
+ *  要求：定义一个int型数组，包含10个元素，
+ */
+
+public class Evaluationoperation {
+    public static void main(String[] args) {
+
+    }
+}
